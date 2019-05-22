92aeae3ba5313688a8aae8f8a5a8639cc424ae67
agrawalvivek2017 <agrawal.vivek2017@gmail.com>
2019-05-22 17:23:21 +0530

    intro section

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
 
-window.onscroll(p) = function() {myFunction()};
+// window.onscroll(p) = function() {myFunction()};
 
-function myFunction() {
-var a=document.body.scrollTop ;
-var b=document.documentElement.scrollTop;
-  if (a > 100 || b > 100) {
-    document.getElementById("imag").className = "slideUp";
-    a=0;
-    b=0;
-  }
-  else
-  document.getElementById("imag").className = "";
+// function myFunction() {
+// var a=document.body.scrollTop ;
+// var b=document.documentElement.scrollTop;
+//   if (a > 100 || b > 100) {
+//     document.getElementById("imag").className = "slideUp";
+//     a=0;
+//     b=0;
+//   }
+//   else
+//   document.getElementById("imag").className = "";
   
-}
+// }
 
 
-// $(window).on('load',function(){
+$(window).on('load',function(){
     
      
-//     $("#imag").fadeOut(-2000);
-//     $("#imag").fadeIn(2000);
-//     // $(".col2").hide();
-//     // $(".col1").hide();
-//     // $(".col3").hide();
-//     // $(".col4").hide();
-//   });
+    $("#imag").fadeOut(-2000);
+    $("#imag").fadeIn(2000);
+    // $(".col2").hide();
+    // $(".col1").hide();
+    // $(".col3").hide();
+    // $(".col4").hide();
+  });
