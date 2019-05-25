b520e584a7286d0fd03c7e6756d1eb43fe740b5e
agrawalvivek2017 <agrawal.vivek2017@gmail.com>
2019-05-25 19:01:10 +0530

    Stats updated

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ function myFunction() {
       $(this).prop('Counter',0).animate({
           Counter: $(this).text()
       }, {
-          duration: 1000,
+          duration: 4000,
           easing: 'swing',
           step: function (now) {
             if(i<2)
